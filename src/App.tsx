58f6a66043e14dc1e6dda5f790bbfe1a0282ae72
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2024-01-21 14:35:21 +0100

    Tailwind CSS install

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import './globals.css'
 
 const App = () => {
-  return <div>Hello</div>
+  return <h1 className="text-4xl font-bold underline">Hello world!</h1>
 }
 
 export default App
